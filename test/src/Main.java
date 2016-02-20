f6e305933fb839ab7f8233a324ca8c22d79d2995
theauda <brionmathieu@gmail.com>
2016-02-20 16:03:31 +0100

    first commit un peu massif ensemble du java utilitaire (ancienne version mais au moin une version utilisable)

---
+++ test/src/Main.java
@@ -0,0 +1,16 @@
+import src.graphique.Fenetre;
+
+/**
+ * Created by mathieu on 20/02/2016.
+ */
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("utilitaire_jggava.Uiiiitilitaire_java.main()");
+
+        Fenetre d=new Fenetre();
+    }
+}
